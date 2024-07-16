9d0edac61f3b69c53e93a3e95ce9d8a1b05e229d
Steve <42746502+StoveTruong@users.noreply.github.com>
2024-07-15 21:30:28 -0500

    first commit backend

---
+++ backend/demo/src/main/java/com/venuenavi/demo/VenuenaviApplication.java
@@ -0,0 +1,13 @@
+package com.venuenavi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenuenaviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenuenaviApplication.class, args);
+	}
+
+}
