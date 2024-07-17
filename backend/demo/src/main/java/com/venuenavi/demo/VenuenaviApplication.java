26724f48dfa2ad2a80ba98f2720502bea686918b
Steve <42746502+StoveTruong@users.noreply.github.com>
2024-07-16 21:32:55 -0500

    Troubleshooting java 22. Tested rest call

--- backend/demo/src/main/java/com/venuenavi/demo/VenuenaviApplication.java
+++ backend/demo/src/main/java/com/venuenavi/demo/VenuenaviApplication.java
@@ -3,6 +3,7 @@ package com.venuenavi.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class VenuenaviApplication {
 
