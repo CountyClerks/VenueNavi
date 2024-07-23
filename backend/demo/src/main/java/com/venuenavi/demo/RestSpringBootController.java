1ab487b01f10dbb3a1fe5edc72361f668b4b41e4
Steve <42746502+StoveTruong@users.noreply.github.com>
2024-07-22 20:34:59 -0500

    Testing gitignore

--- backend/demo/src/main/java/com/venuenavi/demo/RestSpringBootController.java
+++ backend/demo/src/main/java/com/venuenavi/demo/RestSpringBootController.java
@@ -2,6 +2,9 @@ package com.venuenavi.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.net.http.HttpRequest;
 
 @RestController
 public class RestSpringBootController {
@@ -10,5 +13,14 @@ public class RestSpringBootController {
     public String hello() {
         return "Hello world";
     }
+
+    @GetMapping(value = "/callclienthello")
+    private String getHelloClient() {
+        String uri = "http://localhost:8080/hello";
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+        return result;
+    }
+
     
 }
