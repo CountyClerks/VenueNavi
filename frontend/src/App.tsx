f2a435115986cb53da7ae9726d64686e0808edee
Eric Hardy <erichardy011@gmail.com>
2024-08-02 01:11:30 -0500

    import RenderVenue component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import RenderVenue from './components/renderVenue'
+
+// CURRENT DATA FROM .JSON IS JUST EXAMPLES! NOT REAL DATA TO BE USED IN PROD.
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
+     <RenderVenue/>
     </>
   )
 }
