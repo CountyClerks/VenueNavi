120d842a035833f62952f7f28361bf17c4c46ae7
Eric Hardy <erichardy011@gmail.com>
2024-11-20 02:20:48 -0600

    add header links and title

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,6 +1,14 @@
 import './Header.css'
 export const Header = () => {
     return (
-        <p className="header-link">VenueNavi</p>
+        <div className="navbar">
+            <div className="header-left">
+                <p className="title-link">VenueNavi</p>
+            </div>
+            <div className="header-right">
+                <p className="upcoming-link">Upcoming</p>
+                <p className="search-link">Search</p>
+            </div>
+        </div>
     )
 }
